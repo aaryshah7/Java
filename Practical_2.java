5ea3e43ae7346d5a84a82e772acaa61b14ff5048
aaryshah7 <21ce124@charusat.edu.in>
2022-07-31 21:25:17 +0530

    code

---
+++ Practical_2.java
@@ -0,0 +1,19 @@
+//21CE124-Aary Shah
+import java.util.Scanner;
+public class Practical_2{
+    public static void main(String[] args) {
+        String s;
+        Scanner sc = new Scanner(System.in);
+        s = sc.next();
+        char[] ch = s.toCharArray();
+        if(ch[0] == 'o')
+        {
+            System.out.println(ch[0]);
+        }
+        if(ch[1] == 'z')
+        {
+            System.out.println(ch[1]);
+        }
+        System.out.println("21CE124-Aary Shah");
+    }
+}
